250873829cecc185ca3dbb9b44f3e5e6be9eb282
Evgenii Mikhailov <e.mikhailov@outlook.com>
2023-12-19 17:44:37 +0300

    done

--- SwiftUI_task7/ContentView.swift
+++ SwiftUI_task7/ContentView.swift
@@ -4,20 +4,52 @@
 //
 //  Created by Evgenii Mikhailov on 17.12.2023.
 //
-
 import SwiftUI
 
 struct ContentView: View {
+    @State private var isExpanded = false
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        HStack {
+            VStack {
+                ZStack {
+                    Button(action: {
+                        withAnimation(.easeInOut) {
+                            isExpanded.toggle()
+                        }
+                    }, label: {
+                        HStack {
+                            Image(systemName: "arrowshape.backward.fill")
+                                .foregroundStyle(Color.white)
+                            Text("Back")
+                                .font(.system(size: 14, weight: .semibold))
+                                .foregroundStyle(Color.white)
+                        }
+                        .padding()
+                    })
+                    .opacity(isExpanded ? 1 : 0)
+
+                    Button(action: {
+                        withAnimation(.easeInOut) {
+                            isExpanded.toggle()
+                        }
+                    }, label: {
+                        Text("Open")
+                            .foregroundStyle(Color.white)
+                    })
+                    .opacity(isExpanded ? 0 : 1)
+                }
+                Spacer().frame( height:  isExpanded ? 200 : 0)
+            }
+            Spacer().frame( width: isExpanded ? 200 : 0)
         }
-        .padding()
+        .background(Color.blue)
+        .cornerRadius(20)
+        .position(x: isExpanded ? UIScreen.main.bounds.width/2 : UIScreen.main.bounds.width - 100,
+                  y: isExpanded ? UIScreen.main.bounds.height/2 : UIScreen.main.bounds.height - 150)
     }
 }
+
 
 #Preview {
     ContentView()
